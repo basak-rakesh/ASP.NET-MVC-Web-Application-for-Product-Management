48fb7625c5720290413bc78d904d2d7da84a8d95
basak-rakesh <91903788+basak-rakesh@users.noreply.github.com>
2025-07-26 18:39:15 +0530

    Add files via upload

---
+++ Web.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Web.DataAccess.Data;
+using Web.DataAccess.Repository.IRepository;
+using Web.Models;
+
+namespace Web.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product> ,IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
